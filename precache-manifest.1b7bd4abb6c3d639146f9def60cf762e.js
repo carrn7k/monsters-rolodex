5def4b5896cc25b703448c830600f6dc13cd93ad
Heath Carr <carrn7k@gmail.com>
2019-07-15 16:26:14 +0900

    Updates

---
+++ precache-manifest.1b7bd4abb6c3d639146f9def60cf762e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5417562c08e7f86cdad4ee36c8827cb",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e42ea74aff06f322b959",
+    "url": "/monsters-rolodex/static/css/main.8048e767.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "e42ea74aff06f322b959",
+    "url": "/monsters-rolodex/static/js/main.a22a6ead.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
